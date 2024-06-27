00c0c0b96c84fa6206f294d9e2814d6732ac9205
zengo7 <hassanahmadkahrapa@gmail.com>
2024-06-27 15:02:28 +0300

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -156,7 +156,7 @@ renderLargerThumbnail: true
     throw e
   }
 }
-handler.help = ['menu1']
+handler.help = ['mix']
 handler.tags = ['infobot']
 handler.command = ['menu','b','list'] 
 handler.register = false
